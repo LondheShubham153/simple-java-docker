50e0108aed36b55b088f48785bdac1645eeda570
Shubham Londhe <shubhamnath5@gmail.com>
2023-08-05 07:45:53 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello, Docker! Current date: " + currentDate);
+    }
+}
